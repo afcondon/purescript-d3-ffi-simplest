a53a2fc76ffa98f8184f8849f2961a58795bc989
afcondon <andrew.condon@gmail.com>
2020-05-04 21:17:40 +0200

    add in basics of simple chart from observableshq
    type checks but not finished

--- src/Main.js
+++ src/Main.js
@@ -1,32 +1,88 @@
 'use strict'
 
-var GUPSelection
-var svg
+var rootGUP
+var svgGUP
+var rootChart
+var svgChart
 var width = 960
 var height = 480
 var alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
 // ============================ Purescript FFI functions are the only exports ===================================
-exports.initDOMJS = function (DOMroot) {
-  GUPSelection = d3.select(DOMroot)
+exports.initGUP = function (DOMroot) {
+  rootGUP = d3.select(DOMroot)
 
-  svg = GUPSelection.append('svg')
+  svgGUP = rootGUP
+    .append('svg')
     .attr('width', width)
     .attr('height', height)
     .append('g')
     .attr('transform', 'translate(32,100)')
 
   // The initial display.
-  update(alphabet)
+  updateGUPJS(alphabet)
 }
-exports.updateJS = function (ignored) {
-  return update(randomLetters())
+exports.updateGUP = function (ignored) {
+  return updateGUPJS(randomLetters())
 }
 
-var update = function (data) {
+exports.initChart = function (DOMroot) {
+  rootChart = d3.select(DOMroot)
+
+  svgChart = rootChart.attr('width', width).attr('height', height)
+}
+
+exports.drawChart = function (data) {
+  return drawChartJS(data)
+}
+
+// ============================ JS implementation of the chart example  ===================================
+
+var drawChartJS = function (data) {
+  svgChart
+    .append('g')
+    .attr('fill', color)
+    .selectAll('rect')
+    .data(data)
+    .join('rect')
+    .attr('x', (d, i) => x(i))
+    .attr('y', d => y(d.value))
+    .attr('height', d => y(0) - y(d.value))
+    .attr('width', x.bandwidth())
+
+  svgChart.append('g').call(xAxis)
+
+  svgChart.append('g').call(yAxis)
+
+  return svg.node()
+}
+xAxis = g =>
+  g.attr('transform', `translate(0,${height - margin.bottom})`).call(
+    d3
+      .axisBottom(x)
+      .tickFormat(i => data[i].name)
+      .tickSizeOuter(0)
+  )
+
+yAxis = g =>
+  g
+    .attr('transform', `translate(${margin.left},0)`)
+    .call(d3.axisLeft(y).ticks(null, data.format))
+    .call(g => g.select('.domain').remove())
+    .call(g =>
+      g
+        .append('text')
+        .attr('x', -margin.left)
+        .attr('y', 10)
+        .attr('fill', 'currentColor')
+        .attr('text-anchor', 'start')
+        .text(data.y)
+    )
+// ============================ JS implementation of the update function for the GUP example  ===================================
+var updateGUPJS = function (data) {
   var t = d3.transition().duration(750)
 
-  svg
+  svgGUP
     .selectAll('text')
     .data(data, d => d)
     .join(
--- src/Main.purs
+++ src/Main.purs
@@ -6,15 +6,54 @@ import Effect (Effect)
 import Effect.Console (log)
 import Effect.Uncurried (EffectFn1, runEffectFn1)
 
-foreign import initDOMJS :: EffectFn1 String Unit
-foreign import updateJS  :: EffectFn1 Unit Unit
+type ChartObj = { name :: String, value :: Number }
+type ChartData = Array ChartObj
+
+chartData :: ChartData
+chartData = [
+    { name: "E", value: 0.12702 }
+  , { name: "T", value: 0.09056 }
+  , { name: "A", value: 0.08167 }
+  , { name: "O", value: 0.07507 }
+  , { name: "I", value: 0.06966 }
+  , { name: "N", value: 0.06749 }
+  , { name: "S", value: 0.06327 }
+  , { name: "H", value: 0.06094 }
+  , { name: "R", value: 0.05987 }
+  , { name: "D", value: 0.04253 }
+  , { name: "L", value: 0.04025 }
+  , { name: "C", value: 0.02782 }
+  , { name: "U", value: 0.02758 }
+  , { name: "M", value: 0.02406 }
+  , { name: "W", value: 0.0236 }
+  , { name: "F", value: 0.02288 }
+  , { name: "G", value: 0.02015 }
+  , { name: "Y", value: 0.01974 }
+  , { name: "P", value: 0.01929 }
+  , { name: "B", value: 0.01492 }
+  , { name: "V", value: 0.00978 }
+  , { name: "K", value: 0.00772 }
+  , { name: "J", value: 0.00153 }
+  , { name: "X", value: 0.0015 }
+  , { name: "Q", value: 0.00095 }
+  , { name: "Z", value: 0.00074 }
+]
+
+foreign import initChart :: EffectFn1 String Unit
+foreign import drawChart  :: EffectFn1 ChartData Unit
+
+foreign import initGUP :: EffectFn1 String Unit
+foreign import updateGUP  :: EffectFn1 Unit Unit
 
 main :: Effect Unit
 main = do
-  runEffectFn1 initDOMJS "#hook"
+  runEffectFn1 initChart "#chart"
+  runEffectFn1 drawChart chartData
+
+  runEffectFn1 initGUP "#GUP"
   -- | to run updates with D3.transitions you will have to do these calls asynchronously
   -- | this is not an issue for simple charts
-  -- runEffectFn1 updateJS unit
-  -- runEffectFn1 updateJS unit
-  -- runEffectFn1 updateJS unit
+  -- runEffectFn1 updateGUP unit
+  -- runEffectFn1 updateGUP unit
+  -- runEffectFn1 updateGUP unit
   log "🍝"
